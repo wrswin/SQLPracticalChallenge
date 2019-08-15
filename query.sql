7d6555589d3a99531c570599f45f40138b1d4e3d
WAYDE REITSMA <102506005@student.swin.edu.au>
2019-08-16 08:57:39 +1000

    Task 1 Complete

--- query.sql
+++ query.sql
@@ -1,1 +1,20 @@
 -- Wayde Reitsma - 102506005
+
+/*
+
+Tour (TourName, Description)
+Primary Key (TourName)
+
+Client (ClientID, Surname, GivenName, Gender)
+Primary Key (ClientID)
+
+Event (TourName, EventYear, EventMonth, EventDay, Fee)
+Primary Key (TourName, EventYear, EventMonth, EventDay)
+Foreign Key (TourName) Reference Tour
+
+Booking (TourName, EventYear, EventMonth, EventDay, ClientID, DateBooked, Payment)
+Primary Key (TourName, EventYear, EventMonth, EventDay, ClientID)
+Foreign Key (Tourname, EventYear, EventMonth, EventDay) References Event
+Foreign Key (ClientID) References Client
+
+*/
